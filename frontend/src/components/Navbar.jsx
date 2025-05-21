ead6c23955a5f91ac5094a20a7f975c20c5af861
Jeshan khatri <jeshankhatri01@gmail.com>
2025-05-21 14:13:27 +0000

    cartadded

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import {assets} from '../assets/assets'
 import { Link, NavLink }  from 'react-router-dom'
 
 const Navbar = () => {
 
  const [visible, setVisible] = useState(false);
+ const {setShowSearch} = useContext(ShopContext);
 
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
@@ -36,7 +37,7 @@ const Navbar = () => {
 
 
       <div className='flex item-center gap-6'>
-            <img src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
+            <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer' alt="" />
 
             <div className='group relative'>
                  <img className='w-5 cursor-pointer' src={assets.profile_icon} alt=""/>
